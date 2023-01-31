86900f41f9b01a2c93ef7eba3f2a8fa4b108ab7a
notsquirrelcoding <carros0508@rpsmn.org>
2023-01-31 14:34:31 -0600

    cool image

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use num_complex::ComplexFloat;
 // const Z: Complex<f64> = Complex::new(3.0, 2.0);
 
 fn main() -> anyhow::Result<()> {
-    let mut grapher = Grapher::new(3000, 3000, 100.0, 0.0, 0.0, false, |z| {
+    let mut grapher = Grapher::new(4000, 4000, 400.0, 0.0, 0.0, false, |z| {
         1.0 / z.powc(z.tan())
     });
 
