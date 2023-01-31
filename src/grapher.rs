a63c54fb603bc22ef77fd3dee1d9997032e61ce4
notsquirrelcoding <carros0508@rpsmn.org>
2023-01-31 14:32:17 -0600

    a

--- src/grapher.rs
+++ src/grapher.rs
@@ -1,40 +1,72 @@
-use std::path::Path;
+use std::{f64::consts::PI, path::Path};
 
-use image::{ImageBuffer, Rgb, RgbImage, ImageResult};
+use colors_transform::Color;
+use console::Term;
+use image::{ImageBuffer, ImageResult, Rgb, RgbImage};
+use num_complex::Complex;
+
+type ComplexFunc = fn(Complex<f64>) -> Complex<f64>;
 
 pub struct Grapher {
     buf: ImageBuffer<Rgb<u8>, Vec<u8>>,
     width: u32,
     height: u32,
+    zoom_factor: f64,
+    x_shift: f64,
+    y_shift: f64,
+    axis_enabled: bool,
+    f: ComplexFunc,
 }
 
 impl Default for Grapher {
     fn default() -> Self {
-
         let mut buf = RgbImage::new(100, 100);
 
         buf.fill(255);
 
-        Self { buf, width: 100, height: 100 }
+        Self {
+            buf,
+            width: 100,
+            height: 100,
+            zoom_factor: 1.0,
+            x_shift: 0.0,
+            y_shift: 0.0,
+            axis_enabled: false,
+            f: |z| z,
+        }
     }
 }
 
 impl Grapher {
-
-    pub fn new(w: u32, h: u32) -> Self {
+    pub fn new(
+        w: u32,
+        h: u32,
+        zoom_factor: f64,
+        x_shift: f64,
+        y_shift: f64,
+        axis_enabled: bool,
+        f: ComplexFunc,
+    ) -> Self {
         let mut buf = RgbImage::new(w, h);
 
         buf.fill(255);
 
-        Self { buf, width: w, height: h }
+        Self {
+            buf,
+            width: w,
+            height: h,
+            zoom_factor,
+            x_shift,
+            y_shift,
+            axis_enabled,
+            f,
+        }
     }
 
     pub fn draw_axes(&mut self, tick_space: u32) {
-
         let black_pixel = Rgb([0, 0, 0]);
 
         for y in 0..self.height {
-
             if y % tick_space == 0 {
                 self.buf.put_pixel((self.width / 2) + 1, y, black_pixel);
                 self.buf.put_pixel((self.width / 2) - 1, y, black_pixel);
@@ -44,7 +76,6 @@ impl Grapher {
         }
 
         for x in 0..self.width {
-
             if x % tick_space == 0 {
                 self.buf.put_pixel(x, (self.width / 2) + 1, black_pixel);
                 self.buf.put_pixel(x, (self.width / 2) - 1, black_pixel);
@@ -71,7 +102,6 @@ impl Grapher {
         self.height as i32 / 2
     }
 
-
     /// Maps a point from the coordinate system where `(0, 0)` is the center to the system where `(0, 0)` is the top-left corner
     /// of the screen
     pub fn map_point(&self, x: i32, y: i32) -> (u32, u32) {
@@ -81,4 +111,101 @@ impl Grapher {
             (-y + (self.height / 2) as i32) as u32,
         )
     }
+
+    /// Updates the plot
+    pub fn update_plot(&mut self) -> anyhow::Result<()> {
+        // Update very pixel and we add an offset of +-1
+        for x in -(self.width_frac_2())..=((self.width_frac_2()) - 1) {
+            for y in (1 - (self.height_frac_2()))..=(self.height_frac_2()) {
+                let num = Complex::new(
+                    (x as f64 + self.x_shift) / self.zoom_factor,
+                    (y as f64 + self.y_shift) / self.zoom_factor,
+                );
+
+                // Assign a color to f(z)
+
+                let color = color_num((self.f)(num));
+
+                let num = num * self.zoom_factor;
+
+                let point = self.map_point(
+                    (num.re - self.x_shift) as i32,
+                    (num.im - self.y_shift) as i32,
+                );
+
+                self.put_pixel(point.0, point.1, color);
+                // println!("{}/{}", (x+self.width_frac_2()) * ((y+self.height_frac_2())-1) + y + self.height_frac_2(), self.width * self.height);
+            }
+        }
+
+        if self.axis_enabled {
+            self.draw_axes(5);
+        }
+
+        self.save(Path::new("test.png"))?;
+        Ok(())
+    }
+
+    pub fn run(&mut self) -> anyhow::Result<()> {
+        let stdout = Term::buffered_stdout();
+
+        loop {
+            if let Ok(character) = stdout.read_char() {
+                match character {
+                    'z' => {
+                        self.zoom_factor *= 100.0;
+                        self.x_shift *= 100.0;
+                        self.y_shift *= 100.0;
+                    }
+                    'x' => {
+                        self.zoom_factor /= 100.0;
+                        self.x_shift /= 100.0;
+                        self.y_shift /= 100.0;
+                    }
+                    'w' => self.y_shift += 1000.0,
+                    'a' => self.x_shift -= 1000.0,
+                    's' => self.y_shift -= 1000.0,
+                    'd' => self.x_shift += 1000.0,
+                    'e' => self.axis_enabled = !self.axis_enabled,
+                    'r' => {
+                        self.zoom_factor = 1.0;
+                        self.x_shift = 0.0;
+                        self.y_shift = 0.0;
+                    }
+                    'k' => break,
+                    _ => {}
+                }
+                self.update_plot()?;
+
+                print!("{}[2J", 27 as char);
+
+                let num = Complex::new(
+                    self.x_shift / self.zoom_factor,
+                    self.y_shift / self.zoom_factor,
+                );
+
+                println!(
+                    "ZOOM: {}\tCENTER (z): {}\t f(z)={}\tAXIS ENABLED: {}",
+                    self.zoom_factor,
+                    num,
+                    (self.f)(num),
+                    self.axis_enabled
+                );
+            }
+        }
+
+        Ok(())
+    }
+}
+
+/// A function which uses domain coloring to color a pixel which represents a complex number
+fn color_num(num: Complex<f64>) -> Rgb<u8> {
+    let h = (num.arg() + (2.0 * PI) / 3.0) * 50.0;
+    let l = ((2.0 * num.norm().atan()) / PI) * 50.0;
+
+    let hsl = colors_transform::Hsl::from(h as f32, 100.0, l as f32);
+
+    let rgb = hsl.to_rgb().as_tuple();
+
+    Rgb([rgb.0 as u8, rgb.1 as u8, rgb.2 as u8])
 }
--- src/main.rs
+++ src/main.rs
@@ -1,111 +1,14 @@
 mod grapher;
 
-use std::{f64::consts::PI, path::Path};
-
-use colors_transform::Color;
-use console::Term;
 use grapher::Grapher;
-use image::Rgb;
-use num_complex::{Complex, ComplexFloat};
+use num_complex::ComplexFloat;
 
-const Z: Complex<f64> = Complex::new(3.0, 2.0);
+// const Z: Complex<f64> = Complex::new(3.0, 2.0);
 
 fn main() -> anyhow::Result<()> {
-    let mut grapher = Grapher::default();
+    let mut grapher = Grapher::new(3000, 3000, 100.0, 0.0, 0.0, false, |z| {
+        1.0 / z.powc(z.tan())
+    });
 
-    let mut zoom_factor = 20.0;
-    let mut x_shift = 0.0;
-    let mut y_shift = 0.0;
-    let mut axis_enabled = false;
-
-    let stdout = Term::buffered_stdout();
-
-    loop {
-        if let Ok(character) = stdout.read_char() {
-            match character {
-                'z' => {
-                    zoom_factor *= 2.0;
-                    x_shift *= 2.0;
-                    y_shift *= 2.0;
-                }
-                'x' => {
-                    zoom_factor /= 2.0;
-                    x_shift /= 2.0;
-                    y_shift /= 2.0;
-                }
-                'w' => y_shift += 10.0,
-                'a' => x_shift -= 10.0,
-                's' => y_shift -= 10.0,
-                'd' => x_shift += 10.0,
-                'e' => axis_enabled = !axis_enabled,
-                'r' => {
-                    zoom_factor = 1.0;
-                    x_shift = 0.0;
-                    y_shift = 0.0;
-                }
-                _ => break,
-            }
-            update_plot(&mut grapher, zoom_factor, x_shift, y_shift, axis_enabled)?;
-            print!("{}[2J", 27 as char);
-
-            let num = Complex::new(x_shift / zoom_factor, y_shift / zoom_factor);
-
-            println!(
-                "ZOOM: {zoom_factor}\tCENTER (z): {}\t f(z)={}\tAXIS ENABLED: {}",
-                num,
-                f(num),
-                axis_enabled
-            );
-        }
-    }
-
-    Ok(())
-}
-
-fn update_plot(
-    grapher: &mut Grapher,
-    zoom_factor: f64,
-    x_shift: f64,
-    y_shift: f64,
-    draw_axes: bool,
-) -> anyhow::Result<()> {
-    for x in -(grapher.width_frac_2())..=((grapher.width_frac_2()) - 1) {
-        for y in (1-(grapher.height_frac_2()))..=(grapher.height_frac_2()) {
-            let num = Complex::new(
-                (x as f64 + x_shift) / zoom_factor,
-                (y as f64 + y_shift) / zoom_factor,
-            );
-
-            let color = color_num(f(num));
-
-            let num = num * zoom_factor;
-
-            let point = grapher.map_point((num.re - x_shift) as i32, (num.im - y_shift) as i32);
-
-            grapher.put_pixel(point.0, point.1, color);
-        }
-    }
-
-    if draw_axes {
-        grapher.draw_axes(5);
-    }
-
-    grapher.save(Path::new("test.png"))?;
-    Ok(())
-}
-
-/// A function which uses domain coloring to color a pixel which represents a complex number
-fn color_num(num: Complex<f64>) -> Rgb<u8> {
-    let h = (num.arg() + (2.0 * PI) / 3.0) * 50.0;
-    let l = ((2.0 * num.norm().atan()) / PI) * 50.0;
-
-    let hsl = colors_transform::Hsl::from(h as f32, 100.0, l as f32);
-
-    let rgb = hsl.to_rgb().as_tuple();
-
-    Rgb([rgb.0 as u8, rgb.1 as u8, rgb.2 as u8])
-}
-
-fn f(z: Complex<f64>) -> Complex<f64> {
-    1.0 / z.ln()
+    grapher.update_plot()
 }
